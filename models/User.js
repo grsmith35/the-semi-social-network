7154eca1d86b810efd255b2858a6a690573db683
grsmith35 <gordonrileysmith@gmail.com>
2021-12-01 21:15:18 -0700

    building project bones

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: Users
+        }
+    ]
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
